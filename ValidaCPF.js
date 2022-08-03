2434c8ff4a8a2995bdc17387ee75014c2b94f36d
Hudson Retonde <hudsonsretonde@gmail.com>
2022-08-03 20:06:20 -0300

    replace, slice, reduce.

--- ValidaCPF.js
+++ ValidaCPF.js
@@ -24,6 +24,27 @@ function ValidaCPF(cpfEnviado) {
   });
 }
 
+ValidaCPF.prototype.valida = function() {
+  if(typeof this.cpfLimpo === 'undefined') return false;
+  if(this.cpfLimpo.length !== 11) return false;
+  
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+
+  return true;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+
+  let regressivo = cpfArray.length + 1;
+  const digito = cpfArray.reduce((ac, val) => {
+    console.log(regressivo, typeof val, regressivo * val);
+    ac += (ac* Number(val));
+    regressivo--;    
+    return ac;
+  }, 0);
+};
+
 const cpf = new ValidaCPF('705.484.450-52');
-console.log(cpf.cpfLimpo);
-//cpf.valida();
+console.log(cpf.valida());
